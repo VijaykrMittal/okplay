e4e93731e5c155cabacfaccbd48efc5680b31a85
B2C Developer <ravic@biz2credit.com>
2015-07-09 12:13:57 +0000

    Changes made by B2C Developer (ravic@biz2credit.com).

---
+++ okplay/script/home.js
@@ -0,0 +1,168 @@
+(function(global){
+    var homeViewModel
+        app = global.app = global.app || {};
+    
+    homeViewModel = kendo.data.ObservableObject.extend({
+        
+        categorydrawerData:'',
+        categoryName:'',
+        dataListStatus:'',
+        
+        show : function(e)
+        {
+            $('#category-dropdown select').val(0);
+            $('#age-dropdown select').val(0);
+            $('[data-role="drawer"]').children().css("background-color","#373F4A");
+            e.view.scroller.scrollTo(0, 0)
+            
+            var categoryDataSource = new kendo.data.DataSource({
+                transport:{
+                    read:{
+                        url:'script/category.json',
+                        dataType:'json'
+                    }
+                }
+            });
+            categoryDataSource.fetch(function(){
+                var data = this.data();
+                app.homeService.viewModel.setcategoryDataSource(data);
+            });
+            
+        },
+        
+        setcategoryDataSource :function(data)
+        {
+            this.set("categorydrawerData",data);
+        },
+        
+        setcategoryData :function(data)
+        {
+            if(data.length === 0)
+            {
+                this.set("dataListStatus","There is no article.");
+                this.set("listData","");
+            }
+            if(data.length>0)
+            {
+                this.set("dataListStatus","");
+                this.set("listData",data);
+            }
+            
+        },
+        
+        drpdownFilter : function(data)
+        {
+            console.log(data);
+            var categoryDataSource = new kendo.data.DataSource({
+                transport:{
+                    read:{
+                        url:'script/categoryData.json',
+                        dataType:'json'
+                    }
+                },
+                filter:{
+                    logic:"and",
+                    filters:[
+                        { field: "year", operator: "eq", value: data },
+                        { field: "value", operator: "eq", value: sessionStorage.getItem('categorySelectItem') }
+                    ]
+                }
+            });
+            categoryDataSource.fetch(function(){
+                var data = this.view();
+                app.homeService.viewModel.setcategoryData(data);
+            });
+        },
+        
+        ageDrpDownFilter : function(data)
+        {
+            console.log(data);
+            var categoryDataSource = new kendo.data.DataSource({
+                transport:{
+                    read:{
+                        url:'script/categoryData.json',
+                        dataType:'json'
+                    }
+                },
+                filter:{
+                    logic:"and",
+                    filters:[
+                        { field: "value", operator: "eq", value: data },
+                        { field: "year", operator: "eq", value: sessionStorage.getItem('ageSelectItem') }
+                    ]
+                }
+            });
+            categoryDataSource.fetch(function(){
+                var data = this.view();
+                app.homeService.viewModel.setcategoryData(data);
+            });
+        },
+        
+        drawerChildClick : function(e)
+        {
+            var that = this;
+            console.log(e);
+            alert(e['target']['attributes']['data-id'].value);
+            alert(e['target']['attributes']['data-value'].value);
+            
+            if(e['target']['attributes']['data-value'].value === "category")
+            {
+                $('#age-dropdown').css("display","block");
+                $('#category-dropdown').css("display","none");
+                that.set("categoryName",e['target']['attributes']['data-id'].value);
+                app.homeService.viewModel.fetchCategoryData(e['target']['attributes']['data-id'].value);
+                app.mobileApp.navigate("#healthView");
+            }
+            
+            if(e['target']['attributes']['data-value'].value === "age")
+            {
+                $('#category-dropdown').css("display","block");
+                $('#age-dropdown').css("display","none");
+                that.set("categoryName",e['target']['attributes']['data-id'].value);
+                app.homeService.viewModel.fetchAgeData(e['target']['attributes']['data-id'].value);
+                app.mobileApp.navigate("#healthView");
+            }
+        },
+        
+        fetchCategoryData : function(data)
+        {
+            sessionStorage.setItem("categorySelectItem",data);
+            var categoryDataSource = new kendo.data.DataSource({
+                transport:{
+                    read:{
+                        url:'script/categoryData.json',
+                        dataType:'json'
+                    }
+                },
+                filter: { field: "value", operator: "eq", value: data }
+            });
+            categoryDataSource.fetch(function(){
+                var data = this.view();
+                app.homeService.viewModel.setcategoryData(data);
+            });
+        },
+        
+        fetchAgeData : function(data)
+        {
+            console.log(data);
+            sessionStorage.setItem("ageSelectItem",data);
+            var categoryDataSource = new kendo.data.DataSource({
+                transport:{
+                    read:{
+                        url:'script/categoryData.json',
+                        dataType:'json'
+                    }
+                },
+                filter: { field: "year", operator: "eq", value: data }
+            });
+            categoryDataSource.fetch(function(){
+                var data = this.view();
+                app.homeService.viewModel.setcategoryData(data);
+            });
+        }
+        
+    });
+    app.homeService = {
+        viewModel : new homeViewModel()
+    };
+}(window));
--- okplay/script/homepage.js
+++ okplay/script/homepage.js
@@ -20,35 +20,65 @@ app.homepage = (function(){
     
     var homePageViewModel = (function(){
         
+        var showDrawerText = kendo.observable({
+            categoryName:'',
+            listData:'',
+            setDrawerTaskName : function(val)
+            {
+                var that = this;
+                that.set("categoryName",val);
+            },
+            
+            callAPi :function()
+            {
+                alert("callapi");
+                console.log(categoryViewModel.categorydrawerData);
+                this.set("listData",(categoryViewModel.categorydrawerData));
+                console.log(this.get('listData'));
+            }
+        });
+        
         var show = function(e)
         {
+            $('#category-dropdown select').val(0);
+            $('#age-dropdown select').val(0);
             $('[data-role="drawer"]').children().css("background-color","#373F4A");
             e.view.scroller.scrollTo(0, 0)
         };
         
         var drawerChildClick = function(e)
         {
-           console.log(e);
+            console.log(e);
             alert(e['target']['attributes']['data-id'].value);
             alert(e['target']['attributes']['data-value'].value);
             
             if(e['target']['attributes']['data-value'].value === "category")
             {
-                alert("category");
-                $().css("display","block");
+                $('#age-dropdown').css("display","block");
+                $('#category-dropdown').css("display","none");
+                
+                showDrawerText.setDrawerTaskName(e['target']['attributes']['data-id'].value);
+                showDrawerText.callAPi();
                 app.mobileApp.navigate("#healthView");
             }
             
             if(e['target']['attributes']['data-value'].value === "age")
             {
-                alert("age");
+                $('#category-dropdown').css("display","block");
+                $('#age-dropdown').css("display","none");
+                
+                showDrawerText.setDrawerTaskName(e['target']['attributes']['data-id'].value);
+                
+                app.mobileApp.navigate("#healthView");
             }
         };
        
         return {
+            showDrawerText:showDrawerText,
             categorydrawerData:categoryViewModel.categorydrawerData,
             drawerChildClick:drawerChildClick,
-            show:show
+            show:show,
+            listData:showDrawerText.listData
         };
     }());
     return homePageViewModel;
